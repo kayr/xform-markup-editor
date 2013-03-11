90b81ac468a25c8fc09b3ccaf3a5d76350aeca4a
kayr <noryak1234@gmail.com>
2013-03-11 18:52:38 +0300

    XForm Deserialiser.. work in progress

---
+++ markup-parser/src/main/groovy/org/openxdata/markup/serializer/XFormDeserializer.groovy
@@ -0,0 +1,19 @@
+package org.openxdata.markup.serializer
+
+import org.openxdata.markup.Study
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kay
+ * Date: 3/11/13
+ * Time: 7:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class XFormDeserializer {
+
+
+    public Study toStudy(String studyXML){
+
+    }
+
+}
