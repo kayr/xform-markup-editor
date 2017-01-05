db36e25901e1716c3abdb94ac1960e20efc1eb31
kayr <noryak1234@gmail.com>
2017-01-05 10:08:36 +0300

    Improve tooltip rendering

--- markup-ui/src/main/java/org/openxdata/markup/ui/ToolTipTreeRenderer.java
+++ markup-ui/src/main/java/org/openxdata/markup/ui/ToolTipTreeRenderer.java
@@ -1,6 +1,8 @@
 package org.openxdata.markup.ui;
 
+import org.openxdata.markup.IFormElement;
 import org.openxdata.markup.IQuestion;
+import org.openxdata.markup.Page;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -19,6 +21,9 @@ public class ToolTipTreeRenderer extends DefaultTreeCellRenderer {
             if (userObject instanceof IQuestion) {
                 IQuestion qn = (IQuestion) userObject;
                 renderQuestion(cmp, qn);
+            } else if (userObject instanceof Page) {
+                Page p = (Page) userObject;
+                renderPage(cmp, p);
             } else {
                 this.setToolTipText(userObject + "");
             }
@@ -34,6 +39,31 @@ public class ToolTipTreeRenderer extends DefaultTreeCellRenderer {
 
         String tooltip = "#" + qn.getQuestionIdx() + ' ' + qn.getBinding() + '-' + qn.getText();
 
+        this.setText(qn.getNumberedText());
         this.setToolTipText(tooltip);
+    }
+
+    private void renderPage(Component cmp, Page qn) {
+
+        if (qn.getBinding() != null && qn.getIndexedBinding().length() > 64)
+            cmp.setForeground(Color.red);
+
+        String finalText;
+
+        if (qn.getText() != null && !qn.getText().isEmpty()) {
+            finalText = qn.getNumberedText();
+        } else if (qn.getId() != null) {
+            finalText = qn.getQuestionIdx() + ". [" + qn.getId() + "]";
+        } else {
+            IFormElement firstElementsWithId = qn.getFirstElementsText();
+            if (firstElementsWithId != null) {
+                finalText = "Group: " + firstElementsWithId.getNumberedText();
+            } else {
+                finalText = "Group";
+            }
+        }
+
+        this.setText(finalText);
+        this.setToolTipText(finalText);
     }
 }
